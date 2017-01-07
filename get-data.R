e7c61ed9a773d0a5d38c5846673568e7fd981198
Edridge D'Souza <edsouza@umass.edu>
2017-01-07 15:14:07 -0500

    Changed - Now we just get all image coordinates at the beginning

--- get-data.R
+++ get-data.R
@@ -4,25 +4,34 @@ library(httr)
 fileName <- "./TOKEN.txt"
 access.token <- readChar(fileName, file.info(fileName)$size)
 
-getLocationIDs <- function(latitude = 48.858844, longitude = 2.294351) {
+getCoords <- function(latitude = 48.858844, longitude = 2.294351) {
     
   params <- list(access_token = access.token,
-                   lat = latitude,
-                   lng = longitude)
-    r <- GET("https://api.instagram.com/v1/locations/search",
+                   lat = 42.3912, # latitude
+                   lng = -72.5267, # longitude
+                    distance = 5000)
+    #r <- GET("https://api.instagram.com/v1/locations/search",
+    #         query = params)
+      
+    r <- GET("https://api.instagram.com/v1/media/search",
              query = params)
-    
     parsed <- content(r, "parsed")
     data <- parsed$data
     
     numResults <- length(data)
-    placeIDs <-
+    lats <-
       lapply(
         X = data,
-        FUN = function(x) x$id
-      ) %>% as.character()
+        FUN = function(x) x$location$latitude
+      ) %>% as.numeric()
+    longs <- lapply(
+      X = data,
+      FUN = function(x) x$location$longitude
+    ) %>% as.numeric()
     
-    return(placeIDs)
+    coords <- data.frame(latitude = lats, longitude = longs)
+    
+    return(coords)
 }
 
-
+coords <- getCoords(latitude = 42.3912, longitude = -72.5267)
