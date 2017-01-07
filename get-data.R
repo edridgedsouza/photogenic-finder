796d1b34d11d26ca40763047a560cf7eb0f09630
Edridge D'Souza <edsouza@umass.edu>
2017-01-07 16:19:46 -0500

    Summarizer functions

--- get-data.R
+++ get-data.R
@@ -4,7 +4,7 @@ library(httr)
 fileName <- "./TOKEN.txt"
 access.token <- readChar(fileName, file.info(fileName)$size)
 
-getCoords <- function(latitude = 48.858844, longitude = 2.294351) {
+makeRequest <- function(latitude = 48.858844, longitude = 2.294351) {
     
   params <- list(access_token = access.token,
                    lat = 42.3912, # latitude
@@ -17,21 +17,19 @@ getCoords <- function(latitude = 48.858844, longitude = 2.294351) {
              query = params)
     parsed <- content(r, "parsed")
     data <- parsed$data
+}
+
+summarizeRequest <- function(parsedData){
+    pictime <- lapply(X = parsedData, FUN = function(x) x$created_time) %>% as.numeric()
+    lats <- lapply(X = parsedData, FUN = function(x) x$location$latitude) %>% as.numeric()
+    longs <- lapply(X = parsedData, FUN = function(x) x$location$longitude) %>% as.numeric()
+    numlikes <- lapply(X = parsedData, FUN = function(x) x$likes$count) %>% as.numeric()
+    alltags <- lapply(X = parsedData, FUN = function(x) x$tags) %>% as.character()
     
-    numResults <- length(data)
-    lats <-
-      lapply(
-        X = data,
-        FUN = function(x) x$location$latitude
-      ) %>% as.numeric()
-    longs <- lapply(
-      X = data,
-      FUN = function(x) x$location$longitude
-    ) %>% as.numeric()
-    
-    coords <- data.frame(latitude = lats, longitude = longs)
+    coords <- data.frame(time = pictime, latitude = lats, longitude = longs, likes = numlikes, tags = alltags)
     
     return(coords)
 }
 
-coords <- getCoords(latitude = 42.3912, longitude = -72.5267)
+requestData <- makeRequest(latitude = 42.3912, longitude = -72.5267)
+imageData <- summarizeRequest(requestData)
